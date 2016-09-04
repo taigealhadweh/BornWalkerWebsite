bdd40dd9f6f28d96f7cb732f158f331f28364e9e
Taige Alhadweh <taigealhadweh@gmail.com>
2016-09-04 17:28:33 +1000

    Added login and register function. Once logged in, displays successful login message (need to redirect back home). Once registered, displays successful registration message (needs to redirect back home).

---
+++ code/bootstrap-3.3.7-dist/db_const.php
@@ -0,0 +1,7 @@
+<?php
+	# mysql db constants DB_HOST, DB_USER, DB_PASS, DB_NAME
+	const DB_HOST = 'localhost:8889';
+	const DB_USER = 'root';
+	const DB_PASS = 'root';
+	const DB_NAME = 'php_mysql_login_system';
+?>
