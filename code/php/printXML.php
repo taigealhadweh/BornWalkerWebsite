7ecfc9a407c74248ff986eb95704c3a1c71a264b
Taige Alhadweh <taigealhadweh@gmail.com>
2016-09-11 00:24:40 +1000

    Added uvIndex variable to the Session variables.

--- code/php/printXML.php
+++ code/php/printXML.php
@@ -1,14 +1,21 @@
 <?php
-    
+session_start();   
 $url = "http://www.arpansa.gov.au/uvindex/realtime/xml/uvvalues.xml";
 $xml = simplexml_load_file($url); 
 foreach ($xml->location as $location)
 {
     if($location['id'] == 'melbourne'){
         print_r("The uv index is: ");
-        echo $location->index;
+        
+        $uvIndex = $location->index->asXML();
+        print_r($uvIndex);
+        $_SESSION['uvIndex'] = $uvIndex;
+
+//print_r("Session is: $_SESSION[uvIndex]" );
+//        echo $location->index;
         
     }
     
 }
+
 ?>
