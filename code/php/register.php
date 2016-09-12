90bb35245c789598f844991caa944253d49a2642
Taige Alhadweh <taigealhadweh@gmail.com>
2016-09-12 10:54:34 +1000

    Cleaned code.

--- code/php/register.php
+++ code/php/register.php
@@ -1,98 +1,3 @@
-
-
-
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- this is for the size, so if mobile show mobile size and if ipad show ipad size etc -->
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>BornWalker</title>
-    <!-- Bootstrap -->
-    <link href="../bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/custom.css" rel="stylesheet">
-
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-    <div class="container">
-        <div class="row-fluid">
-            <div class="column-padding">
-
-                <form>
-                    <h1>BornWalker Registration</h1>
-                    <div class="row">
-                        <label class="col-sm-2 col-form-label">Name:</label>
-                        <input type="text" name="userName">
-                    </div>
-                    <div class="row">
-                        <label class="col-sm-2 col-form-label">Password:</label>
-                        <input type="password" name="pwv">
-                    </div>
-                    <div class="row">
-                        <label class="col-sm-2 col-form-label">Confirm password:</label>
-                        <input type="password" name="cpwv">
-                    </div>
-                    <div class="row">
-                        <label class="col-sm-2 col-form-label">Email:</label>
-                        <input type="text" name="mailv">
-                    </div>
-                    <div class="row">
-                        <label class="col-sm-2 col-form-label">Phone:</label>
-                        <input type="text" name="pv">
-                    </div>
-                    <div class="row">
-                        <label class="col-form-label"></label>
-                        <input type="submit" value="Register">
-                    </div>
-                    <h3>Already registered?&nbsp<a href="login.php">Login here</a></h3>
-                </form>
-            </div>
-        </div>
-    </div>
-    <!-- navbar code should be same on all pages -->
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only"> Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <!-- Need to resize the logo to fit in line with the navbar -->
-                <!-- <a href="" class="navbar-left"><img src=""></a> -->
-            </div>
-            <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="../index.html">Home</a></li>
-                    <li><a href="../html/mapRadius.php">Take a walk</a></li>
-                    <li><a href="profile.php">Profile</a></li>
-                    <li class="active"><a href="login.php">Login</a></li>
-
-                </ul>
-            </div>
-        </div>
-    </div>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-
-
-
-</body>
-
 <?php
 	if(isset($_GET['userName'])&&isset($_GET['pwv'])&&isset($_GET['cpwv'])&&isset($_GET['mailv'])&&isset($_GET['pv']))
 	{
@@ -189,4 +94,99 @@
 		}
 	return $_string;
     }
+?>
 
+
+
+
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- this is for the size, so if mobile show mobile size and if ipad show ipad size etc -->
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>BornWalker</title>
+        <!-- Bootstrap -->
+        <link href="../bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/custom.css" rel="stylesheet">
+
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row-fluid">
+                <div class="column-padding">
+
+                    <form>
+                        <h1>BornWalker Registration</h1>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Name:</label>
+                            <input type="text" name="userName">
+                        </div>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Password:</label>
+                            <input type="password" name="pwv">
+                        </div>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Confirm password:</label>
+                            <input type="password" name="cpwv">
+                        </div>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Email:</label>
+                            <input type="text" name="mailv">
+                        </div>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Phone:</label>
+                            <input type="text" name="pv">
+                        </div>
+                        <div class="row">
+                            <label class="col-form-label"></label>
+                            <input type="submit" value="Register">
+                        </div>
+                        <h3>Already registered?&nbsp<a href="login.php">Login here</a></h3>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- navbar code should be same on all pages -->
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only"> Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- Need to resize the logo to fit in line with the navbar -->
+                    <!-- <a href="" class="navbar-left"><img src=""></a> -->
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="../index.html">Home</a></li>
+                        <li><a href="../html/mapRadius.php">Take a walk</a></li>
+                        <li><a href="profile.php">Profile</a></li>
+                        <li class="active"><a href="login.php">Login</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+
+
+    </body>
