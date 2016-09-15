6175aefdf2ca43b18090739b16480275c0ca1f6c
Ziyi Li <zli230@student.monash.edu>
2016-09-15 22:12:36 +1000

    This is a blank page with only the background colour

---
+++ code/php/takeaWalk.php
@@ -0,0 +1,10 @@
+<html>
+<body style = "background: #FF5F6D;
+background: -webkit-linear-gradient(to left, #FF5F6D , #FFC371);
+background: linear-gradient(to left, #FF5F6D , #FFC371)">
+<h1></h1>
+</body>
+</html>
+
+
+        
