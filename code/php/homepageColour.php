20b73fe82627252dec4d315ed90f2da0537da93d
Ziyi Li <zli230@student.monash.edu>
2016-09-17 22:30:03 +1000

    Possible front page background colour

---
+++ code/php/homepageColour.php
@@ -0,0 +1,7 @@
+<html>
+<body style = "background: #FF5F6D;
+background: -webkit-linear-gradient(to left, #FF5F6D , #FFC371);
+background: linear-gradient(to left, #FFEDBC , #ED4264)">
+<h1></h1>
+</body>
+</html>
