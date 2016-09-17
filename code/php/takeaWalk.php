c11e984a7dc045663a7ae54c996870e266b0b200
Ziyi Li <zli230@student.monash.edu>
2016-09-17 22:55:44 +1000

    possible background colour for take a walk page

--- code/php/takeaWalk.php
+++ code/php/takeaWalk.php
@@ -1,7 +1,7 @@
 <html>
 <body style = "background: #FF5F6D;
 background: -webkit-linear-gradient(to left, #FF5F6D , #FFC371);
-background: linear-gradient(to left, #FF5F6D , #FFC371)">
+background: linear-gradient(to right, #FF5F6D , #FFC371)">
 <h1></h1>
 </body>
 </html>
