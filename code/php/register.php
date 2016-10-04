1af2711fa68f2bbfd6088f53c81c589231136e5c
Ziyi Li <zli230@student.monash.edu>
2016-10-04 11:26:25 +1100

    Modified layout. Added header and footer

--- code/php/register.php
+++ code/php/register.php
@@ -1,11 +1,15 @@
 <?php
+include("headerforphp.php");
+?>
+
+
+<?php
 	if(isset($_GET['userName'])&&isset($_GET['pwv'])&&isset($_GET['cpwv'])&&isset($_GET['mailv']))
 	{
 		$name=checkUserName($_GET['userName']);
 		$password=checkPassword($_GET['pwv']);
 		$conpassword=$_GET['cpwv'];
 		$mail=checkemail($_GET['mailv']);
-//		$phone=checkPhone($_GET['pv']);
 	
 		if($password===$conpassword)
 		{
@@ -60,18 +64,6 @@
     }
     
     /**
-	 * checkPhone() check the format of this phone number
-	 * @param string $_string the phone number entered by user.
-	 * @return string $_string the checked phone number.
-	 */
-//    function checkPhone($_string) {
-//		if (!preg_match('/^[0-9]{9,10}$/',$_string)) {
-//			location('Invalid phone number, please try again :)','register.php');
-//		}
-//	return $_string;
-//    }
-    
-    /**
 	 * checkUserName() check the format of this user name
 	 * @param string $_string the user name entered by user.
 	 * @return string $_string the checked user name.
@@ -96,32 +88,7 @@
     }
 ?>
 
-
-
-
-    <html>
-
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- this is for the size, so if mobile show mobile size and if ipad show ipad size etc -->
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>BornWalker</title>
-        <!-- Bootstrap -->
-        <link href="../bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../css/custom.css" rel="stylesheet">
-
-
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    </head>
-
-    <body>
+    <header>
         <div class="container">
             <div class="row-fluid">
                 <div class="column-padding">
@@ -159,36 +126,5 @@
                 </div>
             </div>
         </div>
-        <!-- navbar code should be same on all pages -->
-        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only"> Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <!-- Need to resize the logo to fit in line with the navbar -->
-                    <!-- <a href="" class="navbar-left"><img src=""></a> -->
-                </div>
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="../index.html">Home</a></li>
-                        <li><a href="../html/mapRadius.php">Take a walk</a></li>
-                        <li><a href="profile.php">Profile</a></li>
-                        <li class="active"><a href="login.php">Login</a></li>
-
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-
-
-
-    </body>
+    </header>
+    <?php include("footer.php") ?>
