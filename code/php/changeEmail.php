8e73e88f5f9486e1041b2aeb1e802721f8fbb551
Taige Alhadweh <taigealhadweh@gmail.com>
2016-10-04 18:31:28 +1100

    Added change email functionality in the settings page.

--- code/php/changeEmail.php
+++ code/php/changeEmail.php
@@ -8,7 +8,16 @@ $userName = $_SESSION['name'];
 <header>
     <div class="header-content">
         <div class="header-content-inner">
+            <h1>Change email</h1>
+            <form method="post" action="changeUserInformationQuery.php">
             
+            <hr>
+            
+            <input class="form-control" type="text" id="newPassword" name="newEmail" placeholder="New email" style="background-color:rgba(255, 226, 223, 0.6);border:0px">
+                <button type="submit" class="btn btn-primary">Change email</button>  
+                
+                </form>
+                    
             
 
         </div>
--- code/php/changeUserInformationQuery.php
+++ code/php/changeUserInformationQuery.php
@@ -9,10 +9,10 @@
        mysqli_query($conn, $query);
    }
 
-if(isset($_POST['email']))
+if(isset($_POST['newEmail']))
    {
        session_start();
-       $newEmail = $_POST['email'];
+       $newEmail = $_POST['newEmail'];
        $conn = mysqli_connect("40.126.240.245", "k10838a", "password","bornWalkerMap");
        $query = "update user set email = $newEmail where userid = $_SESSION[user_id]";
        mysqli_query($conn, $query);
