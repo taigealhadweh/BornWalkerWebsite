a3ee7a49f099af0be480e95c8265b26b3206ef7c
Zhe90 <babalalol123@gmail.com>
2016-10-09 17:54:47 +1100

    Add start and end button to count walking time.

--- code/listPlaces.php
+++ code/listPlaces.php
@@ -47,13 +47,97 @@ if (logged_in()== true) {
         <label style='color:rgba(255, 208, 209, 1)' for="end">End</label>
         <input class="form-control" type="text" id="end" placeholder="Pick a marker or enter an address" style="background-color:rgba(255, 226, 223, 0.6);border:0px">
         <button id="route" class="dropbtn" >Get me there</button>
-        <button id="startTimer" class="dropbtn" onclick="startTimer">Start timer</button>
-        <button id="startTimer" class="dropbtn" onclick="endTimer">Finish walk</button>
-    
+		
+          <!--   <button id="startTimer" class="dropbtn" onclick="startTimer">Start timer</button>
+        <button id="startTimer" class="dropbtn" onclick="endTimer">Finish walk</button>?-->
+		<form  method="post" >
+  
+			<input type="submit" name="starttimer" value="Start">  
+			<input type="submit" name="endtimer" value="Finish">
+  
+		</form>
+		
+		<p>Time:</p><p id='timewindow'> <?php 
+		$conn = mysqli_connect("40.126.240.245", "k10838a", "password", "bornWalkerMap")
+	or die("<p>Unable to connect to the database server1.</p>"
+	. "<p>Error code " . mysqli_connect_errno()
+	. ": " . mysqli_connect_error()) . "</p>";
+	
+		$showquery="select period from walk  WHERE walkid = ( SELECT MAX(walkid) FROM bornWalkerMap.walk ) ";
+		$showtimeresault=mysqli_query($conn, $showquery) or die('search error' . mysqli_error());
+		
+	  if(!$showtimeresault )
+			{
+			  die('Could not get data: ' . mysqli_error());
+			}
+		while($rowshow = mysqli_fetch_assoc($showtimeresault))
+		{
+			$period=$rowshow['period'];
+			Echo $period;
+			
+		}
+		?> </p>
     </div>
     <div class="col-lg-7" id="map" style="border:0px"></div>
 </div>        
-<?php include("php/footer.php") ?>
+<?php
+	$userid=$_SESSION['user_id'];
+if (isset($_POST["starttimer"])) {
+	$conn = mysqli_connect("40.126.240.245", "k10838a", "password", "bornWalkerMap")
+	or die("<p>Unable to connect to the database server1.</p>"
+	. "<p>Error code " . mysqli_connect_errno()
+	. ": " . mysqli_connect_error()) . "</p>";
+	
+	$starttimequery="insert into bornWalkerMap.walk(starttime,userid) values (current_timestamp(),'$userid')";
+	$starttimeresult = mysqli_query($conn, $starttimequery) or die('search error' . mysqli_error());
+	
+	
+}
+
+if (isset($_POST["endtimer"])) {
+	$conn = mysqli_connect("40.126.240.245", "k10838a", "password", "bornWalkerMap")
+	or die("<p>Unable to connect to the database server1.</p>"
+	. "<p>Error code " . mysqli_connect_errno()
+	. ": " . mysqli_connect_error()) . "</p>";
+	
+	$widquery="SELECT walkid FROM bornWalkerMap.walk WHERE walkid = ( SELECT MAX(walkid) FROM bornWalkerMap.walk )";
+	/*$widquery="SELECT MAX(walkid) FROM bornWalkerMap.walk WHERE userid='41'";*/
+	$widresult = mysqli_query($conn, $widquery) or die('search error' . mysqli_error());
+	
+	  if(!$widresult )
+			{
+			  die('Could not get data: ' . mysqli_error());
+			}
+		while($row1 = mysqli_fetch_assoc($widresult))
+			{	
+			$walkid = $row1['walkid']; 
+			/*$walkid = $row1['walkid'];*/
+			$endtimequery="UPDATE bornWalkerMap.walk SET endtime =current_timestamp() WHERE walkid='$walkid'";
+			$endtimeresult = mysqli_query($conn, $endtimequery) or die('search error' . mysqli_error());
+			
+			$ctquery= "select endtime, starttime from bornWalkerMap.walk  WHERE walkid='$walkid'";
+			$ctresult=mysqli_query($conn, $ctquery)or die('search error' . mysqli_error());
+			
+			if(!$ctresult )
+			{
+			  die('Could not get data: ' . mysqli_error());
+			}
+			while($row2 = mysqli_fetch_assoc($ctresult))
+			{	
+			$endtime = $row2['endtime']; 
+			$starttime = $row2['starttime'];
+			$period = strtotime($endtime) - strtotime($starttime);
+			
+			$periodquery="UPDATE bornWalkerMap.walk SET period ='$period' WHERE walkid='$walkid'";
+			$periodresult = mysqli_query($conn, $periodquery)or die('search error' . mysqli_error());
+			
+			}
+		
+			} 
+			
+}
+
+ include("php/footer.php") ?>
 <script>
     $(document).ready(function () {
         checkDropDownValue('<?php if(isset($_POST["dpText"])){ echo $_POST["dpText"];} ?>')
